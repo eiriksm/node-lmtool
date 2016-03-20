1bd8fa77784c9b261686f3eceb737388f11f65e0
Eirik S. Morland <eirik@morland.no>
2016-03-20 15:37:55 +0100

    Write tests for downloadArchive module.

--- test/test.js
+++ test/test.js
@@ -1,8 +1,26 @@
 'use strict';
+const proxyquire = require('proxyquire').noPreserveCache();;
 const fs = require('fs');
 const path = require('path');
+const EventEmitter = require('events').EventEmitter;
+const util = require('util');
+
+function StubEmitter() {
+  EventEmitter.call(this);
+}
+util.inherits(StubEmitter, EventEmitter);
+StubEmitter.prototype.pipe = function () {
+  // noop.
+};
 
 require('should');
+
+let stub;
+let requestStub = {
+  get: () => {
+    return stub
+  }
+}
 
 describe('downloadArchive module', function() {
   it('Should export a function', () => {
@@ -18,6 +36,18 @@ describe('downloadArchive module', function() {
       fs.readFileSync(path.join(process.cwd(), d)).toString().should.equal('testResponse');
       done(err);
     });
+  });
+  it('Should call the callback only once, even if we force it to do something stupid', (done) => {
+    stub = new StubEmitter();
+    proxyquire('./../src/downloadArchive', {
+      'request': requestStub
+    })('http://localhost:8866/1234.tar.gz', (err, d) => {
+      done(err);
+    });
+    setTimeout(_ => {
+      stub.emit('end');
+      stub.emit('error', new Error('problem'));
+    }, 100);
   });
 });
 
