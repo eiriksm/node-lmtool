5764fd6d716c6d800d23aeb4a427264accc7c3af
Eirik S. Morland <eirik@morland.no>
2016-03-23 06:42:45 +0100

    Simplify index module some.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 'use strict';
-const url = 'http://www.speech.cs.cmu.edu/cgi-bin/tools/lmtool/run';
 const async = require('async');
+
 const uploadText = require('./src/uploadText');
 const findArchive = require('./src/findArchive');
 const downloadArchive = require('./src/downloadArchive');
 const extractArchive = require('./src/extractArchive');
+
+const url = 'http://www.speech.cs.cmu.edu/cgi-bin/tools/lmtool/run';
+
 module.exports = function createDictionary(words, callback) {
   async.waterfall([
     uploadText.bind(null, url, words),
     findArchive,
     downloadArchive,
     extractArchive
-  ], function(e) {
-    callback(e);
-  });
+  ], callback);
 };
